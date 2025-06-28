700e482303de9ea2822c0054629f99fbffba379b
Danishsakthi8072 <danishsakthi8072@gmail.com>
2025-06-28 23:42:43 +0530

    Add files via upload

---
+++ Capstone_Restaurant_Ordering_System/src/main/java/Restaurant_Ordering_System/exception/EmptyOrderException.java
@@ -0,0 +1,7 @@
+package main.java.Restaurant_Ordering_System.exception;
+
+public class EmptyOrderException extends Exception {
+    public EmptyOrderException(String message) {
+        super(message);
+    }
+}
